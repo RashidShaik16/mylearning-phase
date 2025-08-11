ab76d0a57d79c912226dafc61e3c0159fd57a3da
Rashid Shaik <rashidshaik.dev16@gmail.com>
2025-08-11 14:34:58 +0530

    Resolved the images not loading bug

--- javascript/DOM Challenges/KBC/tailwind.config.js
+++ javascript/DOM Challenges/KBC/tailwind.config.js
@@ -4,13 +4,26 @@ module.exports = {
     "./index.html",
     "./src/**/*.{html,js}"
   ],
+    safelist: [
+    'bg-kbc-bg',
+    'bg-kbc-curtain',
+    'bg-kbc-logo'
+  ],
   theme: {
     extend: {
-      
+      backgroundImage: {
+         'kbc-bg': "url('./images/background.png')",
+         'kbc-curtain': "url('./images/curtain.png')",
+        'kbc-logo': "url('./images/kbc-logo.png')"
+      }
     },
   },
   plugins: [],
 }
+
+
+
+
 
 
 
