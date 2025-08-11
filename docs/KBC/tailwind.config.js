abddf367e712c2f0c1fff488d2e45af478c68c84
Rashid Shaik <rashidshaik.dev16@gmail.com>
2025-08-11 13:41:34 +0530

    checking the url path to load images

--- docs/KBC/tailwind.config.js
+++ docs/KBC/tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
     "./index.html",
     "./src/**/*.{html,js}"
   ],
+    safelist: [
+    'bg-kbc-bg',
+    'bg-kbc-curtain',
+    'bg-kbc-logo'
+  ],
   theme: {
     extend: {
-      
+      backgroundImage: {
+         'kbc-bg': "url('/images/background.png')",
+         'kbc-curtain': "url('./images/curtain.png')",
+        'kbc-logo': "url('../images/kbc-logo.png')"
+      }
     },
   },
   plugins: [],
