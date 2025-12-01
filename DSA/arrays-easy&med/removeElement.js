a3669ecff8e0f4cf10de0209fceb1977cce25663
Rashid Shaik <rashidshaik.dev16@gmail.com>
2025-12-01 11:47:10 +0530

    Added new problem removeElement

--- DSA/arrays-easy&med/removeElement.js
+++ DSA/arrays-easy&med/removeElement.js
@@ -9,3 +9,16 @@ Return k.
 */
 
 
+function removeElement(nums, val){
+    let index = 0
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[index] = nums[i]
+            index++
+        }
+    }
+    console.log(index, nums)
+}
+
+removeElement([0, 0, 1, 1, 2, 3, 3, 3, 4, 4, 5, 5, 6], 3)
