b585bdb336614210a7e93580a013fdaa468c289f
Rashid Shaik <rashidshaik.dev16@gmail.com>
2025-12-03 09:43:46 +0530

    create http-server in node.js

--- node.js/http-server/index.js
+++ node.js/http-server/index.js
@@ -1,35 +1,30 @@
 const http = require("node:http")
-const fs = require("fs")
 
 const server = http.createServer((req, res) => {
     const method = req.method
     const path = req.url
 
-    const log = `[${Date.now()}] : ${method} ${path}\n`
-    fs.appendFileSync("log.txt", log, "utf-8")
-
-
     switch(method){
-        case "GET" : {
-            switch(path){
-                case "/" : return res.writeHead(200).end("You are at homepage")
-                case "/contact" : return res.writeHead(200).end("You can contact us at 8019734321 or rashidshaik.dev16@gmail.com")
-                case "/about" : return res.writeHead(200).end("I'm an aspiring fullstack developer")
-                case "/tweet" : return res.writeHead(200).end("Tweet-1\nTweet-2\nand so on....")
+        case "GET" : 
+            switch(path) {
+                case "/" : return res.writeHead(200).end("Welcome to the homepage")
+                case "/about" : return res.writeHead(200).end("I'm an aspiring software engineer")
+                case "/contact" : return res.writeHead(200).end("You can contact me at 8019734321")
+                default: return res.writeHead(404).end("The page doesn't exists")
             }
-        }
-        break
-
         case "POST" : {
-            switch(path){
-                case "/tweet" : return res.writeHead(201).end("Your tweet was created successfully")
+            switch(path) {
+                case "/apply" : return res.writeHead(201).end("We received your application")
+                case "/offer" : return res.writeHead(201).end("Thank you for accepting the offer")
+                default: return res.writeHead(400).end("Please apply in our job portal first")
             }
         }
-
-        return res.writeHead(404).end("Invalid request")
     }
+
 })
 
-const PORT = 8000
 
-server.listen(PORT, () => console.log("Server connected successfully"))
+server.listen(8000, function() {
+    console.log("Server connected successfully");
+    
+})
